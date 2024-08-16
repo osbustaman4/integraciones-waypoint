4089c83d62d11d435c90eb6499449ccf6e5a049f
osbustaman4 <osvaldo.bustamante.n@gmail.com>
2024-08-16 14:13:00 -0400

    se agrega validacion

--- integrations/waypoints.py
+++ integrations/waypoints.py
@@ -23,97 +23,99 @@ class Waypoint():
             if not data_timezone:
                 raise ValueError("Error al obtener la zona horaria")
 
-            query_results = response_gs_objects(data_timezone, 1743)
+            query_results = response_gs_objects(data_timezone, 1)
             if not query_results:
                 raise ValueError("Error al obtener los datos de la consulta")
 
             for query in query_results:
 
-                payload = [
-                            {
-                                "fecha": query.fecha,
-                                "latitud": query.latitud,
-                                "longitud": query.longitud,
-                                "altitud": query.altitud,
-                                "velocidad": query.velocidad,
-                                "cog": query.cog,
-                                "nsat": query.nsat,
-                                "realtime": True,
-                                "input": [
-                                    0,
-                                    0,
-                                    0,
-                                    0
-                                ],
-                                "hdop": 0.0,
-                                "ignicion": 0,
-                                "adc": [
-                                    -200.0,
-                                    -200.0,
-                                    -200.0,
-                                    -200.0
-                                ],
-                                "power": 0,
-                                "horometro": 0,
-                                "odometro": 0,
-                                "panico": 0,
-                                "bateria": 0.0,
-                                "bateriaInt": 0.0,
-                                "patente": query.patente,
-                                "tercerojo": 0,
-                                "aceleracion": 0,
-                                "frenada": 0,
-                                "giro": 0
+                if query.nsat > 3:
+                    
+                    payload = [
+                                {
+                                    "fecha": query.fecha,
+                                    "latitud": query.latitud,
+                                    "longitud": query.longitud,
+                                    "altitud": query.altitud,
+                                    "velocidad": query.velocidad,
+                                    "cog": query.cog,
+                                    "nsat": query.nsat,
+                                    "realtime": True,
+                                    "input": [
+                                        0,
+                                        0,
+                                        0,
+                                        0
+                                    ],
+                                    "hdop": 0.0,
+                                    "ignicion": 0,
+                                    "adc": [
+                                        -200.0,
+                                        -200.0,
+                                        -200.0,
+                                        -200.0
+                                    ],
+                                    "power": 0,
+                                    "horometro": 0,
+                                    "odometro": 0,
+                                    "panico": 0,
+                                    "bateria": 0.0,
+                                    "bateriaInt": 0.0,
+                                    "patente": query.patente,
+                                    "tercerojo": 0,
+                                    "aceleracion": 0,
+                                    "frenada": 0,
+                                    "giro": 0
+                                }
+                        ]
+
+                    url = "https://api.waypoint.cl/inbound/inboundLD"
+
+                    payload = json.dumps(payload)
+                    headers = {
+                        'Content-Type': 'application/json',
+                    }
+
+                    response = requests.request("POST", url, headers=headers, data=payload)
+
+                    if response.status_code == 200:
+
+                        code_response = json.loads(response.json())
+                        if int(code_response["code"]) == 0:
+                            
+                            query_exist = get_data_integraciones_sinc(query.imei)
+
+                            data_integraciones_sinc = {
+                                "sinc_integ": load_data('INTEGRATION_NAME'),
+                                "sinc_imei": query.imei,
+                                "sinc_dt_tracker": query.fecha_tracker,
+                                "sinc_dt_server": query.dt_server,
+                                "sinc_params": query.params,
+                                "sinc_lat": query.latitud,
+                                "sinc_lng": query.longitud,
+                                "sinc_speed": query.velocidad,
+                                "sinc_angle": query.angle,
+                                "sinc_plate": query.patente,
+                                "idpoint": 0
                             }
-                    ]
 
-                url = "https://api.waypoint.cl/inbound/inboundLD"
+                            if query_exist.rowcount == 0:
+                                insert_integraciones_sinc(data_integraciones_sinc)
+                                Logger.add_to_log("success", f"insert: {query.patente}", load_data('LOG_DIRECTORY'), "log_waypoint")
 
-                payload = json.dumps(payload)
-                headers = {
-                    'Content-Type': 'application/json',
-                }
+                            else:
+                                update_integraciones_sinc(data_integraciones_sinc)
+                                Logger.add_to_log("success", f"update: {query.patente}", load_data('LOG_DIRECTORY'), "log_waypoint")
 
-                response = requests.request("POST", url, headers=headers, data=payload)
-
-                if response.status_code == 200:
-
-                    code_response = json.loads(response.json())
-                    if int(code_response["code"]) == 0:
-                        
-                        query_exist = get_data_integraciones_sinc(query.imei)
-
-                        data_integraciones_sinc = {
-                            "sinc_integ": load_data('INTEGRATION_NAME'),
-                            "sinc_imei": query.imei,
-                            "sinc_dt_tracker": query.fecha_tracker,
-                            "sinc_dt_server": query.dt_server,
-                            "sinc_params": query.params,
-                            "sinc_lat": query.latitud,
-                            "sinc_lng": query.longitud,
-                            "sinc_speed": query.velocidad,
-                            "sinc_angle": query.angle,
-                            "sinc_plate": query.patente,
-                            "idpoint": 0
-                        }
-
-                        if query_exist.rowcount == 0:
-                            insert_integraciones_sinc(data_integraciones_sinc)
-                            Logger.add_to_log("success", f"insert: {query.patente}", load_data('LOG_DIRECTORY'), "log_waypoint")
-
+                            Logger.add_to_log("success", f"punto enviado: {query.patente}", load_data('LOG_DIRECTORY'), "log_waypoint")
                         else:
-                            update_integraciones_sinc(data_integraciones_sinc)
-                            Logger.add_to_log("success", f"update: {query.patente}", load_data('LOG_DIRECTORY'), "log_waypoint")
 
-                        Logger.add_to_log("success", f"punto enviado: {query.patente}", load_data('LOG_DIRECTORY'), "log_waypoint")
+                            error_integracion = response.json()
+                            Logger.add_to_log("error", error_integracion, load_data('LOG_DIRECTORY'), "log_waypoint")
+                            Logger.add_to_log("error", traceback.format_exc(), load_data('LOG_DIRECTORY'), "log_waypoint")
+
                     else:
-
-                        error_integracion = response.json()
-                        Logger.add_to_log("error", error_integracion, load_data('LOG_DIRECTORY'), "log_waypoint")
-                        Logger.add_to_log("error", traceback.format_exc(), load_data('LOG_DIRECTORY'), "log_waypoint")
-
-                else:
-                    raise ValueError("Error al enviar los datos al servidor")
+                        raise ValueError("Error al enviar los datos al servidor")
         
         except ValueError as ex:
             Logger.add_to_log("error", str(ex), load_data('LOG_DIRECTORY'), "log_waypoint")
